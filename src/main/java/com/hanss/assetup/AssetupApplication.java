cd6ce8b348421cec9fccebcaf5f1dd7529a2b4fd
hanss2006 <hanss@live.ru>
2021-04-08 21:57:47 +0300

    Initiate commit

---
+++ src/main/java/com/hanss/assetup/AssetupApplication.java
@@ -0,0 +1,13 @@
+package com.hanss.assetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanss/assetup/AssetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanss.assetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
