49124e9793a3b62ba86610479fe6d4c73a2ba4db
hanss2006 <hanss@live.ru>
2021-12-12 19:30:52 +0300

    Initial commit

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,3 +1,5 @@
+SET JAVA_HOME="C:\Progra~1\Java\jdk-17.0.1"
+
 @REM ----------------------------------------------------------------------------
 @REM Licensed to the Apache Software Foundation (ASF) under one
 @REM or more contributor license agreements.  See the NOTICE file
